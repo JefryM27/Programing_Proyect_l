53eff5790be467df0f1d342c44ed602bf267f157
Jefry Morera <jefrymoreraespinoza09@gmail.com>
2023-07-04 19:34:01 -0600

    Initial Proyect

---
+++ Proyecto_l_Programacion/src/main/java/logica/Proyecto_l_Programacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package logica;
+
+/**
+ *
+ * @author jefry
+ */
+public class Proyecto_l_Programacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
